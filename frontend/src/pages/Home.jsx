5d2f6f41cbc7e21a326f84380ad75d50dbf304f9
amariths <amar.mehdi@iths.se>
2023-05-24 16:33:46 +0200

    a

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+function Home() {
+  return (
+    <div>
+<h1>hej</h1>
+    </div>
+  )
+}
+
+export default Home
---
+++ frontend/src/pages/profile.jsx
@@ -0,0 +1,11 @@
+
+
+function profile() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default profile
