35091ae5df73112102705abe920d451feb9433f5
Fanny <fannyhedmans@gmail.com>
2023-06-01 10:35:37 +0200

    Added name to profileInfo

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -64,19 +64,6 @@ function Posts() {
     };
 
 
-    const [name, setName] = useState('')
-
-    useEffect(() => {
-      axios.get(`http://localhost:8800/accounts/${id}`)
-      .then(response => {
-        setName(response.data.name)
-      })
-      .catch(error => {
-        console.error(error)
-      })
-    }, [id])
-
-
   return (
     <div>
         <Group>
@@ -96,7 +83,6 @@ function Posts() {
       </ButtonGroup>
     </Form>
 
-    <p>{name}</p>
 
 <CardGroup>
 
